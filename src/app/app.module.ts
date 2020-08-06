5ca1facdc4103a50b27aea943789741ac1e3b3b1
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-06 17:12:14 -0500

    Agregando animaciones y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,

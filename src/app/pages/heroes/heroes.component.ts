57cfba17e2ea8536648cfb5d6677434d14462387
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-07 01:45:02 -0500

    Funcionalidad de eliminar Heroe

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faPlusCircle, faSpinner, faExclamationTriangle, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import {HeroesService} from "../../services/heroes.service";
 import {HeroeModel} from "../../../models/heroe.model";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-heroes',
@@ -23,6 +24,29 @@ export class HeroesComponent implements OnInit {
     this.heroeService.getHeroes().subscribe( (resp: any) => {
       this.heroes = resp;
     } );
+  }
+
+  borrarHeroe( heroe: HeroeModel, i: number){
+    Swal.fire({
+      title: '¿Esta seguro?',
+      text: `¿Desea eliminar a ${heroe.nombre}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.heroes.splice(i, 1);
+        this.heroeService.borrarHeroe(heroe.id).subscribe();
+        Swal.fire(
+          'Eliminado',
+          'Es heroe ha sido eliminado',
+          'success'
+        );
+      }
+    })
   }
 
 }
